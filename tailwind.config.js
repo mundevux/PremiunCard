4f860bbd4d500d11b8af164d62779f1b38fbc82a
Camilo Cuitiva <camiloandres174@gmail.com>
2022-02-25 11:07:28 -0500

    Update project PremiumCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: '#565264',
         transparent: 'transparent',
+        secondary: '#614da7',
+        tertiary: '#a79fcc',
       }
     },
   },
